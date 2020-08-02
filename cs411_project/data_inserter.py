5eb391bdb4ed7b0ca5afd139e68c13e901ca4193
Abhinav <abhi.penguin@gmail.com>
2020-08-02 17:13:36 +0530

    Updated data inserter for trips

--- cs411_project/data_inserter.py
+++ cs411_project/data_inserter.py
@@ -1,5 +1,6 @@
 from django.db import connection
 import random
+import string
 import names
 
 domains = [ "hotmail.com", "gmail.com", "aol.com", "mail.com" , "mail.kz", "yahoo.com"]
@@ -27,16 +28,69 @@ def gen_phone_num():
         n = str(random.randint(10**9, 10**10-1))
     return n[:3] + n[3:6] + n[6:]
 
+def gen_random_trip_id():
+    char_set = string.ascii_uppercase + string.digits
+    return ''.join(random.sample(char_set*12, 12))
+
+#I think it makes more sense if the ratings 
+# somewhat agree with eachother
+def gen_rating_with_arg(x):
+    return min(100, x + random.randint(-10, 10))
+
+def gen_rating():
+    return random.randint(5, 100)
+
+
+def dictfetchall(cursor):
+    "Return all rows from a cursor as a dict"
+    columns = [col[0] for col in cursor.description]
+    return [
+        dict(zip(columns, row))
+        for row in cursor.fetchall()
+    ]
+
+
 def add_data(num):
 
     with connection.cursor() as cursor:
+        # Input driver info
         for i in range(num):
             email = generate_random_email()
             # Add account to user account
             cursor.execute("Insert into User_Accounts values (%s, %s, %s)",
             [email, "ananmay", "driver"])
-
             # Add Driver Info
             cursor.execute("Insert into Driver_Info values (%s, %s, %s, %s, %s, %s, %s)",
             [email, names.get_first_name(), names.get_last_name(), gen_phone_num(),
             gen_phone_num(), random.choice(cities), random.choice(cities)])
+
+        # Input Industry Info
+        for i in range(num):
+            email = generate_random_email()
+            # Add account to user account
+            cursor.execute("Insert into User_Accounts values (%s, %s, %s)",
+            [email, "ananmay", "industry"])
+            # Add Industry Info
+            cursor.execute("Insert into Ind_Info values (%s, %s, %s, %s)",
+                           [email, names.get_first_name(), names.get_last_name(), gen_phone_num()
+                            ])
+
+        # Input Trips Info
+        for i in range(num):
+            trip_id = gen_random_trip_id()
+            rating_from_driver = gen_rating()
+            rating_from_ind = gen_rating_with_arg(rating_from_driver)
+            
+            cursor.execute("SELECT emailid FROM Driver_Info")
+            drivers = cursor.fetchall()
+            d_len = len(drivers)
+            cursor.execute("SELECT emailid FROM Ind_Info")
+            industries = cursor.fetchall()
+            i_len = len(industries)
+
+            cursor.execute("Insert Into Trips values (%s, %s, %s, %s, %s, %s, %s, %s)",
+            [trip_id, '1', drivers[random.randint(0,d_len-1)][0],
+             industries[random.randint(0, i_len-1)][0], str(rating_from_driver),
+             str(rating_from_ind), "alright", "okay"])
+            
+
